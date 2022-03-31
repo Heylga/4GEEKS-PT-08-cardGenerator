f526bc4b9f55e27f242e0b45159a9c29417b05d5
Heylga <oy.golovina@gmail.com>
2022-03-31 13:02:37 +0000

    changes in css & js

--- src/app.js
+++ src/app.js
@@ -6,40 +6,54 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let icons = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let cardheader = document.querySelector(".header");
+  let cardcenter = document.querySelector(".main");
+  let cardfooter = document.querySelector(".footer");
+
+  function Cardgenerator() {
+    let randomIcon = Math.floor(Math.random() * icons.length);
+    let getRandomIcon = icons[randomIcon];
+
+    let randomNumber = Math.floor(Math.random() * numbers.length);
+    let getRandomNumber = numbers[randomNumber];
+
+    cardheader.innerHTML = getRandomIcon;
+    cardcenter.innerHTML = getRandomNumber;
+    cardfooter.innerHTML = getRandomIcon;
+  }
+  Cardgenerator();
+
+  // 1 option
+  // icons.forEach(icons) => {var Red = (icons == "♥") || (icons == "♦") ? color: red;}
+
+  // 2 option
+
+  // function cardColor() {
+  //   if (icons == "♥" || icons == "♦") {
+  //     document.getElementsByClassName("header").style.color = "red";
+  //     document.getElementsByClassName("footer").style.color = "red";
+  //   } else {
+  //     document.getElementsByClassName("header").style.color = "black";
+  //     document.getElementsByClassName("footer").style.color = "black";
+  //   }
+
+  //   cardColor();
+  // }
 };
-
-let icons = ["♦", "♥", "♠", "♣"];
-let numbers = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-];
-
-let cardheader = document.querySelector(".header");
-let cardcenter = document.querySelector(".main");
-let cardfooter = document.querySelector(".footer");
-
-function Cardgenerator() {
-  let randomIcon = Math.floor(Math.random() * icons.length);
-  let getRandomIcon = icons[randomIcon];
-
-  let randomNumber = Math.floor(Math.random() * numbers.length);
-  let getRandomNumber = numbers[randomNumber];
-
-  cardheader.innerHTML = getRandomIcon;
-  cardcenter.innerHTML = getRandomNumber;
-  cardfooter.innerHTML = getRandomIcon;
-}
-Cardgenerator();
